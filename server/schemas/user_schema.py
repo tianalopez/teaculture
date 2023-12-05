82f27e7657dd2603a00794357053bd7818f7faa4
Tiana Lopez <lopez.y.tiana@gmail.com>
2023-12-04 19:35:23 -0800

    adds user_community_schema

--- server/schemas/user_schema.py
+++ server/schemas/user_schema.py
@@ -1,14 +1,16 @@
 from marshmallow import fields, validate
 from models.user import User
 from app_setup import ma
+from schemas.recipe_schema import RecipeSchema
 
 
 class UserSchema(ma.SQLAlchemySchema):
     class Meta:
         model = User
         load_instance = True
-        fields = ["id", "username", "email", "location", "interests", "bio"]
+        fields = ["id", "username", "email", "location", "interests", "bio", "recipes"]
 
     username = fields.String(required=True, validate=validate.Length(min=3, max=20))
     password_hash = fields.String(validate=validate.Length(min=12, max=50))
     email = fields.String(required=True, validate=validate.Length(min=2, max=256))
+    recipes = fields.List(fields.Nested(RecipeSchema)) #!check for recursion
