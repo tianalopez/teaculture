4e518a362416ebe7a8ffe8e4cfa3f68a1dfd0ec3
Tiana Lopez <lopez.y.tiana@gmail.com>
2023-12-05 14:41:54 -0800

    adds recipe schema

--- server/schemas/recipe_schema.py
+++ server/schemas/recipe_schema.py
@@ -2,5 +2,27 @@ from marshmallow import fields, validate, validates, ValidationError
 from models.recipe import Recipe
 from app_setup import ma
 
+
 class RecipeSchema(ma.SQLAlchemySchema):
-    pass
+    class Meta:
+        model = Recipe
+        load_instance = True
+        fields = [
+            "id",
+            "title",
+            "instructions",
+            "tags",
+            "ingredients",
+            "medicinal",
+            "creator_id",
+            "created_at",
+            "updated_at",
+        ]
+
+    title = fields.String(required=True, validate=validate.Length(min=2, max=80))
+    instructions = fields.String(
+        required=True, validate=validate.Length(min=3, max=5000)
+    )
+    tags = fields.String(required=True, validate=validate.Length(min=1))
+    ingredients = fields.String(required=True, validate=validate.Length(min=1))
+    medicinal = fields.Boolean(required=True)
