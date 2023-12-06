0acb969bf2e430aa597d9282f24a50ca2a13a562
Tiana Lopez <lopez.y.tiana@gmail.com>
2023-12-05 19:32:02 -0800

    adds new route to user_community

--- server/routes/user_communities.py
+++ server/routes/user_communities.py
@@ -4,9 +4,27 @@ from schemas.user_community_schema import UserCommunitySchema
 from app_setup import db
 
 user_communities_schema = UserCommunitySchema(many=True, session=db.session)
+user_community_schema = UserCommunitySchema(session=db.session)
 
 
 class UserCommunities(Resource):
     def get(self):
         user_communities = user_communities_schema.dump(UserCommunity.query)
         return user_communities, 200
+
+    def post(self):
+        try:
+            data = request.json
+            #validate data
+            user_communities_schema.validate(data)
+            #deserialize with load()
+            new_uc = user_communities_schema.load(data)
+            #add new uc to uc table
+            db.session.add(new_uc)
+            db.session.commit()
+            #serialize with dump()
+            serialized_uc = user_communities_schema.dump(new_uc)
+            return serialized_uc, 201
+        except Exception as e:
+            db.session.rollback()
+            return {'error': str(e)}, 400
