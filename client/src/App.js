3c1a1f0f6be1af02deceaa5fa68ec9b7b9a1c643
Tiana Lopez <lopez.y.tiana@gmail.com>
2023-12-07 12:53:59 -0800

    adds routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,26 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useCallback } from 'react';
+import { Outlet, useNavigate } from "react-router-dom";
+import Navbar from "./components/navbar";
+import AlertBar from "./components/alertbar";
+import HomePage from "./pages/homePage";
+import Authentication from "./pages/authentication";
+import Footer from "./components/footer";
+
 
 const App = () => {
+  const navigate = useNavigate();
+  const [alert, setAlert] = useState(null);
+  const [alertType, setAlertType] = useState("");
+  const [user, setUser] = useState(null)
+
+  const updateUser = (user) => setUser(user);
+
+  const handleNewAlert = useCallback((alert) => {
+    setAlert(alert);
+  }, []);
+
+  const handleAlertType = (type) => setAlertType(type);
+
   return (
     <div>App</div>
   )
