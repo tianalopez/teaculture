ce0d5a2c2c2f1e456b9c189fd736f8d493996b79
Tiana Lopez <lopez.y.tiana@gmail.com>
2023-12-09 22:09:29 -0800

    adds imports to app and moves gapi

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './auth/authProvider'
+import { AuthProvider } from './auth/authProvider';
+import { gapi } from 'gapi-script';
+import {useEffect } from "react";
 
+//page imports
 import Welcome from './pages/welcome';
 import Login from './pages/login';
 import Recipes from './pages/recipes';
@@ -15,6 +18,24 @@ import Error404 from './pages/error404';
 import RequireAuth from './auth/requireAuth';
 
 function App() {
+
+  useEffect(() => {
+    const start = async () => {
+      try {
+        await gapi.client.init({
+          clientId: clientId,
+          scope: ""
+        });
+        console.log("Google API initialized successfully");
+      } catch (error) {
+        console.error("Error initializing Google API:", error);
+        // Handle the initialization error as needed
+      }
+    };
+
+    gapi.load('client:auth2', start);
+  }, []);
+
   return (
   <>
   <AuthProvider>
