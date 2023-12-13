d7e7a2f72210800505b05fbefdf1b5b9b345e899
Tiana Lopez <lopez.y.tiana@gmail.com>
2023-12-13 11:09:17 -0800

    adds formik and yup to form

--- client/src/components/recipeForm.js
+++ client/src/components/recipeForm.js
@@ -7,8 +7,10 @@ import DoneIcon from '@mui/icons-material/Done'
 import { MedicinalSwitch } from '../styles/SwitchStyles';
 import * as yup from 'yup';
 import { useFormik } from "formik";
+import {useAuth} from "../auth/authProvider"
 
 const RecipeForm = () => {
+  const auth = useAuth()
   const [chipStates, setChipStates] = useState(Array(5).fill(false))
   const randomImage = [
     "/images/img1.jpg",
@@ -30,112 +32,151 @@ const RecipeForm = () => {
     });
   };
 
+  const recipeSchema = yup.object().shape({
+    title: yup
+      .string()
+      .required("Please enter a recipe title"),
+    instructions: yup
+      .string()
+      .required("Please enter in some instructions"),
+    tags: yup
+      .string()
+      .required('Please select at least one tag'),
+    ingredients: yup
+      .string()
+      .required('Please put at least one ingredient'),
+    medicinal: yup
+      .boolean()
+      .required('Please indicate if your drink has medicinal properties'),
+    image: yup
+      .string()
+      .required('Please select one image')
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      instructions: '',
+      tags: '',
+      ingredients: '',
+      medicinal: null,
+      image: '',
+    },
+    validationSchema: recipeSchema,
+    onSubmit: (values, {resetForm}) => {
+      console.log('build out a fetch POST', 'submitted')
+      resetForm()
+    }
+  })
+
   return (
-    <Grid className='recipe-container' container spacing={2}>
-      <Grid item xs={12}>
-        <TextField
-          label='Add Title'
-          defaultValue='Add Title'
-          helperText='Cannot leave title blank'
-        />
-        <Button >
-          Add Recipe
-        </Button>
-      </Grid>
-      <Grid sx={{ pl: 2, }} item xs={4}>
-        <Paper sx={{ p: 3 }} elevation={4}>
-          <Typography variant='h6'>
-            Add Ingredients
-          </Typography>
-          <List sx={{ ml: 1, mr: 1 }} className='ingredients-list'>
-            {[...Array(10).keys()].map((index) => (
-              <ListItem key={index} sx={{ pl: 0, pr: 0 }}>
-                <ListItemIcon >
-                  <CoffeeTwoToneIcon />
-                </ListItemIcon>
-                <ListItemText>
-                  <TextField
-                    fullWidth
-                    placeholder="Enter ingredient"
-                    variant='standard'
-                  />
-                </ListItemText>
-              </ListItem>
-            ))}
-          </List>
-        </Paper>
-      </Grid>
-      <Grid sx={{ pl: 2, pr: 2 }} item xs={8}>
-        <Paper sx={{ p: 3 }} elevation={4}>
-          <Typography variant='h6'>
-            Add Instructions
-          </Typography>
-          <List sx={{ ml: 1, mr: 1 }} className='ingredients-list'>
-            {[...Array(10).keys()].map((index) => (
-              <ListItem key={index}>
-                <ListItemIcon >
-                  <EditIcon />
-                </ListItemIcon>
-                <ListItemText>
-                  <TextField
-                    fullWidth
-                    placeholder="Enter instruction"
-                    variant='standard'
-                  />
-                </ListItemText>
-              </ListItem>
-            ))}
-          </List>
-        </Paper>
-      </Grid>
-      <Grid sx={{ pr: 2, mb: 2 }} item xs={12}>
-        <Paper sx={{ p: 3 }} elevation={4}>
-          <Grid container spacing={2}>
-            <Grid item xs={9}>
-              <Typography variant='h6'>Pick Cover Image</Typography>
-              <ImageList sx={{ width: '100%' }} cols={4} rowHeight={164}>
-                {randomImage.map((image) => (
-                  <ImageListItem key={image}>
-                    <img
-                      srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                      alt='drink'
-                      src={`${image}?w=164&h=164&fit=crop&auto=format`}
-                      loading='lazy'
+    <form onSubmit={formik.handleSubmit}>
+      <Grid className='recipe-container' container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            label='Add Title'
+            defaultValue='Add Title'
+            helperText='Cannot leave title blank'
+          />
+          <Button >
+            Add Recipe
+          </Button>
+        </Grid>
+        <Grid sx={{ pl: 2, }} item xs={4}>
+          <Paper sx={{ p: 3 }} elevation={4}>
+            <Typography variant='h6'>
+              Add Ingredients
+            </Typography>
+            <List sx={{ ml: 1, mr: 1 }} className='ingredients-list'>
+              {[...Array(10).keys()].map((index) => (
+                <ListItem key={index} sx={{ pl: 0, pr: 0 }}>
+                  <ListItemIcon >
+                    <CoffeeTwoToneIcon />
+                  </ListItemIcon>
+                  <ListItemText>
+                    <TextField
+                      fullWidth
+                      placeholder="Enter ingredient"
+                      variant='standard'
                     />
-                  </ImageListItem>
-                ))}
-              </ImageList>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography variant='h6'>Select Relevant Tags</Typography>
-              <Grid sx={{ mt: 1, mb: 4 }} container spacing={2}>
-                {chipStates.map((isFilled, index) => (
-                  <Grid item key={index}>
-                    <Chip
-                      label={tags[index]}
-                      onClick={() => handleChipClick(index)}
-                      variant='outlined'
-                      icon={isFilled ? <DoneIcon /> : null}
-                      style={{ padding: '15px', fontSize: '18px', borderColor: '#9C9C9C', backgroundColor: isFilled ? 'rgba(172, 207, 201, 0.5)' : '' }}
+                  </ListItemText>
+                </ListItem>
+              ))}
+            </List>
+          </Paper>
+        </Grid>
+        <Grid sx={{ pl: 2, pr: 2 }} item xs={8}>
+          <Paper sx={{ p: 3 }} elevation={4}>
+            <Typography variant='h6'>
+              Add Instructions
+            </Typography>
+            <List sx={{ ml: 1, mr: 1 }} className='ingredients-list'>
+              {[...Array(10).keys()].map((index) => (
+                <ListItem key={index}>
+                  <ListItemIcon >
+                    <EditIcon />
+                  </ListItemIcon>
+                  <ListItemText>
+                    <TextField
+                      fullWidth
+                      placeholder="Enter instruction"
+                      variant='standard'
                     />
-                  </Grid>
-                ))}
+                  </ListItemText>
+                </ListItem>
+              ))}
+            </List>
+          </Paper>
+        </Grid>
+        <Grid sx={{ pr: 2, mb: 2 }} item xs={12}>
+          <Paper sx={{ p: 3 }} elevation={4}>
+            <Grid container spacing={2}>
+              <Grid item xs={9}>
+                <Typography variant='h6'>Pick Cover Image</Typography>
+                <ImageList sx={{ width: '100%' }} cols={4} rowHeight={164}>
+                  {randomImage.map((image) => (
+                    <ImageListItem key={image}>
+                      <img
+                        srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                        alt='drink'
+                        src={`${image}?w=164&h=164&fit=crop&auto=format`}
+                        loading='lazy'
+                      />
+                    </ImageListItem>
+                  ))}
+                </ImageList>
               </Grid>
-              <FormControlLabel
-                label="Medicinal Drink"
-                labelPlacement='start'
-                sx={{ ml: 0 }}
-                control={<MedicinalSwitch
-                  name="medicinal"
-                // checked={searchObj.medicinal}
-                // onChange={onChange}
-                />}
-              />
+              <Grid item xs={3}>
+                <Typography variant='h6'>Select Relevant Tags</Typography>
+                <Grid sx={{ mt: 1, mb: 4 }} container spacing={2}>
+                  {chipStates.map((isFilled, index) => (
+                    <Grid item key={index}>
+                      <Chip
+                        label={tags[index]}
+                        onClick={() => handleChipClick(index)}
+                        variant='outlined'
+                        icon={isFilled ? <DoneIcon /> : null}
+                        style={{ padding: '15px', fontSize: '18px', borderColor: '#9C9C9C', backgroundColor: isFilled ? 'rgba(172, 207, 201, 0.5)' : '' }}
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
+                <FormControlLabel
+                  label="Medicinal Drink"
+                  labelPlacement='start'
+                  sx={{ ml: 0 }}
+                  control={<MedicinalSwitch
+                    name="medicinal"
+                  // checked={searchObj.medicinal}
+                  // onChange={onChange}
+                  />}
+                />
+              </Grid>
             </Grid>
-          </Grid>
-        </Paper>
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
+    </form>
   )
 }
 
