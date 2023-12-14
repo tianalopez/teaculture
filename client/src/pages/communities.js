5ae426f5d57efcddf29c10c11de147225dbbdf9a
Tiana Lopez <lopez.y.tiana@gmail.com>
2023-12-14 12:40:51 -0800

    hooks up individual community page

--- client/src/pages/communities.js
+++ client/src/pages/communities.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+import CommunityCard from '../components/communityCard';
 import { Box, Grid, Button, Typography, Rating, Paper, } from '@mui/material';
 
 const Communities = () => {
-  const [communities, setCommunities] = useState()
+  const [communities, setCommunities] = useState([])
 
   useEffect(() => {
     fetch('/communities')
@@ -10,13 +11,24 @@ const Communities = () => {
     .then(setCommunities)
     .catch(err => console.log(err))
   },[])
-  console.log(communities)
+
+  const communityCards = communities && communities.map((community) => (
+    <Grid key={community.id} item xs={12} sm={6} md={4} lg={3}>
+      <CommunityCard key={community.id} community={community}/>
+    </Grid>
+  ))
+
   return (
     <Box sx={{ flexGrow: 1, ml: 4, mr: 4, mt: 8 }}>
-      <Grid container spacing={2}>
+      <Grid sx={{ml:0}} container spacing={2}>
         <Grid item xs={12}>
           <Typography variant='h3'>SipHub</Typography>
           <Typography variant='h6'>Create connections with other tea and coffee lovers!</Typography>
+        </Grid>
+        <Grid sx={{ mt: 4, p: 2, justifyContent: 'center' }} item xs={12}>
+          <Grid sx={{ml:0}} container spacing={2}>
+            {communityCards}
+          </Grid>
         </Grid>
       </Grid>
     </Box>
--- client/src/pages/communityPage.js
+++ client/src/pages/communityPage.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import { Box, Grid, Button, Typography, Rating, Paper, } from '@mui/material';
+import React, {useState, useEffect} from 'react';
+import { useParams, useNavigate } from "react-router-dom";
+import { Box, Grid, Button, Typography, Rating, Paper, CircularProgress } from '@mui/material';
+
+
 const CommunityPage = () => {
+  const {id} = useParams()
+  const [community, setCommunity] = useState()
+
+  useEffect(() => {
+    Promise.all([
+      fetch(`/communities/${id}`).then((r) => r.json())
+    ])
+    .then((data) => setCommunity(data[0]))
+    .catch(err => console.log(err))
+  },[id])
+
+  if (!community ) {
+    return <CircularProgress />
+  }
+  console.log(community)
+  console.log(id)
   return (
     <Box sx={{ flexGrow: 1, ml: 4, mr: 4, mt: 8 }}>
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Typography variant='h3'>SipHub</Typography>
-          <Typography variant='h6'>Create Connections with other tea and coffee lovers!</Typography>
+          <Typography variant='h3'>
+            {community.name}
+          </Typography>
+        </Grid>
+        <Grid item xs={9}>
+          <Typography>Filter</Typography>
+        </Grid>
+        <Grid sx={{ p: 2, backgroundColor: 'white', justifyContent: 'center' }} item xs={3}>
+          <Grid container spacing={2}>
+          </Grid>
         </Grid>
       </Grid>
+
     </Box>
   )
 }
