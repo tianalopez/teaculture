066392888dac795d935a6c692ab4e0702e58dd29
Tiana Lopez <lopez.y.tiana@gmail.com>
2023-12-13 21:57:08 -0800

    adds dialogue file

---
+++ client/src/components/confirmDialogue.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { DialogContentText, DialogContent, DialogTitle, DialogActions, Dialog, Button} from '@mui/material';
+
+const ConfirmDialogue = () => {
+  return (
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous
+            location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+  )
+}
+
+export default ConfirmDialogue
